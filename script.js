734177bf0739012a84a4624e38c51366d0fcce0d
SosukeAizen833 <Spandan833@gmail.com>
2021-11-06 17:52:53 +0530

    new

--- script.js
+++ script.js
@@ -4,46 +4,8 @@ let cur = 0
 let first_num=0
 let second_num=0;
 let operator;
-function display_num(num){  
-    cur = cur*10+parseFloat(num)
-    displayer.textContent = cur
-}
-
-function calculate(){
-    let result = operate(first_num,second_num,operator);
-    first_num = result;
-    second_num = 0;
-    displayer.textContent = result
-}
 
 
-
-let num_btns = document.querySelectorAll('.num-btn')
-console.log(num_btns.length)
-
-for(let i=0;i<num_btns.length;i++){
-    num_btns[i].addEventListener('click',()=>{
-        display_num(num_btns[i].textContent)
-        console.log(num_btns[i].textContent)
-        
-    })
-}
-
-let op_btns = document.querySelectorAll('.op-btn');
-for(let i=0;i<op_btns.length;i++){
-    op_btns[i].addEventListener('click',()=>{
-        first_num = parseFloat(first_num)+parseFloat(second_num);
-        second_num = 0;
-        cur = second_num
-        displayer.textContent = first_num
-        operator = op[i].textContent
-
-    })
-}
-
-document.getElementById('equals').addEventListener('click',()=>{
-    calculate()
-});
 
 function get_first(){
     first_num = parseInt(displayer.textContent)
